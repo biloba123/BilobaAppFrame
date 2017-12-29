1fda710ed8c754d34875fc885882eab2af723972
biloba <biloba12345@gmail.com>
2017-12-29 21:58:36 +0800

    color, dimen and layout of ev

---
+++ frame/src/main/java/com/lvqingyang/frame/EmptyView.kt
@@ -0,0 +1,13 @@
+package com.lvqingyang.frame
+
+/**
+ * 一句话功能描述
+ * 功能详细描述
+ * @author Lv Qingyang
+ * @see 相关类/方法
+ * @since
+ * @date 2017/12/29
+ * @email biloba12345@gamil.com
+ * @github https://github.com/biloba123
+ * @blog https://biloba123.github.io/
+ */
